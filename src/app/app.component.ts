59165b8cbe3cc58fcc955f7429bdbb71f8a27ba0
Bohdan Hrynkiv <grynkiv0x001@gmail.com>
2022-06-15 12:09:52 +0300

    Counter added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  counter: number = 0;
+
+  increase(): void {
+    this.counter++;
+  }
 }
